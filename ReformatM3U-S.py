ab46d3d1794424773661241cb06b0237cfdc62f9
Max Rink <mwjrink@gmail.com>
2017-09-01 09:05:57 -0400

    init

--- ReformatM3U-S.py
+++ ReformatM3U-S.py
@@ -1,5 +1,5 @@
 dataMaster = ""
-with open('zelda.m3u', "r") as file:
+with open('playlist.m3u', "r") as file:
 	for data in file:
 		data1 = data.replace("\\", "/")
 		data2 = data1.replace("C:/Users/Max/", "/storage/emulated/0/")
@@ -7,6 +7,6 @@ with open('zelda.m3u', "r") as file:
 			data2 = ""
 		dataMaster = dataMaster + data2
 	file.close()
-with open('zelda.m3u', "w") as file:
+with open('playlist.m3u', "w") as file:
 	file.write(dataMaster)
 	file.close()
